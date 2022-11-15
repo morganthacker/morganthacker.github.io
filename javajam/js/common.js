76e12059e5488819ed25f656ed8902a6bfacb757
MThacker12 <A01463472@usu.edu>
2022-11-14 20:14:35 -0700

    Update common.js

--- javajam/js/common.js
+++ javajam/js/common.js
@@ -1,5 +1,5 @@
 $ (document).ready(function(){
-    $ ("#mug").click(funtion(){
-        alert("JavaJam Mug Club Members get a 10% discount on each cup of coffee!!");
+    $ ("#mug").click(funtion(event){
+        alert("JavaJam Mug Club Members get a 10% discount on each cup of coffee!");
     });
 });
